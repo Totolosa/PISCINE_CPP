571a67ee747bcc5440475c568034df7543cf9df0
tdayde Asusmaison <thomas.dayde@gmail.com>
2021-10-26 18:24:02 +0200

    DAY_07 fini

--- DAY_07/ex02/Array.hpp
+++ DAY_07/ex02/Array.hpp
@@ -1,20 +1,31 @@
-#ifndef ARRAY_TPP
-#define ARRAY_TPP
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
 
 #include <iostream>
 
 template <typename T>
 class Array {
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array const& src);
-		~Array();
+		Array() : _size(0), tab(nullptr) {}
+		Array(unsigned int n) : tab(new T[n]), _size(n) {}
+		Array(Array const& src) : tab(nullptr), _size(0) { *this = src; }
+		~Array() { delete [] tab; }
 
-		unsigned int size() const;
+		unsigned int size() const { return _size; }
 		
-		Array & operator=(Array const& rhs);
-		T & operator[](int i);
+		Array & operator=(Array const& rhs) {
+			if (tab != nullptr)
+				delete tab;
+			tab = new T[rhs.size()];
+			for (unsigned int i = 0; i < rhs._size; i++)
+				tab[i] = rhs.tab[i];
+			return *this;
+		}
+		T & operator[](int i) {
+			if (i < 0 || i >= static_cast<int>(_size))
+				throw std::exception();
+			return tab[i];
+		}
 
 	private:
 		T				*tab;
